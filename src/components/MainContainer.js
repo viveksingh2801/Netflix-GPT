8b36b3c3bf88301e5f88e9394d498404eabd0f93
Vivek Singh <vivek280197@gmail.com>
2024-10-24 07:05:29 +0530

    responsive change

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -4,7 +4,7 @@ import VideoTitle from "./VideoTitle";
 import { useSelector } from "react-redux";
 
 const MainContainer = () => {
-  const movies = useSelector((store) => store.movies.nowPlayingMovies);
+  const movies = useSelector((store) => store.movies?.nowPlayingMovies);
 
   if (!movies) return null;
 
